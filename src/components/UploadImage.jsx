e3561e5f362d0a196d1f508257c8d39af7059b86
deokgoo <deokgoo.kima@oliveyoung.co.kr>
2023-04-30 22:45:38 +0900

    fix: uploadUrl 추가

--- src/components/UploadImage.jsx
+++ src/components/UploadImage.jsx
@@ -3,17 +3,19 @@ import { transImage } from '../util/lego/transImage';
 
 const UploadImage = () => {
   const [file, setFile] = useState(null);
+  const [uploadUrl, setUploadUrl] = useState(null);
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData();
 
     formData.append('image', file);
-    transImage(formData);
+    const workId = await transImage(formData);
+    setUploadUrl(workId);
   }
 
   return (
@@ -22,6 +24,7 @@ const UploadImage = () => {
         <input type="file" onChange={handleFileChange} accept="image/*" />
         <button type="submit">Upload</button>
       </form>
+      {uploadUrl && <a href={`/react-maru_project/#/user/${uploadUrl}`}>{uploadUrl}</a>}
     </div>
   );
 }
--- src/util/lego/transImage.js
+++ src/util/lego/transImage.js
@@ -2,13 +2,9 @@ import axios from 'axios';
 
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
-export const transImage = (formData) => {
+export const transImage = async (formData) => {
   const url = 'https://deokgoo.pythonanywhere.com/upload';
-  axios.post(url, formData)
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  const { data } = await axios.post(url, formData);
+
+  return data;
 }
