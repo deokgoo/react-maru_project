207767d7e39650057dde9c373585a3e87a70d23f
deokgoo <deokgoo.kima@oliveyoung.co.kr>
2023-05-01 13:11:53 +0900

    fix: size 추가

--- src/components/UploadImage.jsx
+++ src/components/UploadImage.jsx
@@ -3,10 +3,15 @@ import { transImage } from '../util/lego/transImage';
 
 const UploadImage = () => {
   const [file, setFile] = useState(null);
+  const [size, setSize] = useState(0);
   const [uploadUrl, setUploadUrl] = useState(null);
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
+  }
+
+  const handleSizeChange = (e) => {
+    setSize(e.target.value);
   }
 
   const handleSubmit = async (e) => {
@@ -14,14 +19,16 @@ const UploadImage = () => {
     const formData = new FormData();
 
     formData.append('image', file);
+    formData.append('size', size)
     const workId = await transImage(formData);
-    setUploadUrl(workId);
+    setUploadUrl(workId, size);
   }
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <input type="file" onChange={handleFileChange} accept="image/*" />
+        <input type="text" onChange={handleSizeChange} />
         <button type="submit">Upload</button>
       </form>
       {uploadUrl && <a href={`/react-maru_project/#/user/${uploadUrl}`}>{uploadUrl}</a>}
