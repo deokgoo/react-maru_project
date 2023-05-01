13604746e85ccb28fad7838d1818452e13bc25f7
deokgoo <deokgoo.kima@oliveyoung.co.kr>
2023-05-01 13:22:48 +0900

    fix: url 수정

--- src/util/lego/transImage.js
+++ src/util/lego/transImage.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
 export const transImage = async (formData) => {
-  const url = 'http://127.0.0.1:5000/upload';
+  const url = 'https://deokgoo.pythonanywhere.com/upload';
   const { data } = await axios.post(url, formData);
 
   return data;
